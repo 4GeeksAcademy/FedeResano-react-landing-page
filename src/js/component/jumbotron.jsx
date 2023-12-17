f84961e4beeaf6c4b7955d6e4923d231daaa0292
Federico Resano <123256603+fede1525@users.noreply.github.com>
2023-12-17 21:11:00 +0000

    commit

---
+++ src/js/component/jumbotron.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import PropTypes from "prop-types";
+
+const Jumbotron = (props) => {
+    return (
+        <div className="jumbotron m-5">
+            <h1 className="display-4">{props.title}</h1>
+            <p>{props.description}</p>
+            <a href={props.buttonURL} className="btn btn-success">{props.buttonLabel}</a>
+        </div>
+    );
+};
+
+Jumbotron.propTypes = {
+    title: PropTypes.string,
+    description: PropTypes.string,
+    buttonLabel: PropTypes.string,
+    buttonUrl: PropTypes.string
+};
+
+ReactDOM.render(<Jumbotron
+    title="A Warm Welcome!"
+    description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+    buttonLabel="Call to action!"
+    buttonURL="https://reactjs.org/"
+/>,
+    document.querySelector("#container")
+);
+
+export default Jumbotron;
