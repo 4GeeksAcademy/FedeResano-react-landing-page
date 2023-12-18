5e1534903210aa92a883c327ab63ce3733f50719
Federico Resano <123256603+fede1525@users.noreply.github.com>
2023-12-18 18:37:08 +0000

    commit

--- src/js/component/footer.jsx
+++ src/js/component/footer.jsx
@@ -2,9 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 const Footer = () => {
-    <footer>
-        Copyright &copy; Your Website 2023
-    </footer>
+    return (
+        <footer>
+            Copyright &copy; Your Website 2023
+        </footer>
+    );
 };
 
 ReactDOM.render(<Footer />, document.querySelector("#Home"));
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -3,7 +3,16 @@ import ReactDOM from "react-dom";
 import PropTypes from "prop-types";
 
 const Home = () => {
-	
+	return(
+        <Navbar />
+        <div className="container">
+            <Jumbotron />
+            <div>
+                {cardArray}
+            </div>
+        </div>
+        <Footer />
+    );
 };
 
 export default Home;
